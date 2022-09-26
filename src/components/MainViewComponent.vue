9b1f7bde9483bef575038b24c5f2d5888911df00
Guilherme Saracura <guilhermesaracura@gmail.com>
2022-09-25 21:27:05 -0300

    Complete the creation of front strucuture of HomePage

--- src/components/MainViewComponent.vue
+++ src/components/MainViewComponent.vue
@@ -1,15 +1,54 @@
 <template>
   <v-main style="background-color:#212123">
     <v-col>
-      <!--<v-row>
-        <h1></h1>
+      <div class="container">
+        <h1 class="text_style">Calculadoras</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListCalculators" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
       </v-row>
-      <v-row v-else >
-        <h1></h1>
-          <v-col>
-            <ModelCard></ModelCard>
-          </v-col>
-      </v-row>-->
+      </div>
+      <div class="container">
+        <h1 class="text_style">Fones</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListHeadphones" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
+      </v-row>
+      </div>
+      <div class="container">
+        <h1 class="text_style">Canetas</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListPens" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
+      </v-row>
+      </div>
+      <div class="container">
+        <h1 class="text_style">Livros</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListBooks" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
+      </v-row>
+      </div>
+      <div class="container">
+        <h1 class="text_style">Teclados</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListKeyboard" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
+      </v-row>
+      </div>
+      <div class="container">
+        <h1 class="text_style">Computadores</h1>
+        <v-row class="row_style">
+        <v-col class="col_style" v-for="prod in this.$store.getters.getListMachines" :key="prod.id">
+            <ModelCard :prod="prod"></ModelCard>
+        </v-col>
+      </v-row>
+      </div>
     </v-col>
   </v-main>
 </template>
@@ -19,10 +58,49 @@
 import ModelCard from "@/components/ModelCard"
 export default {
     name: 'MainViewComponent',
-    components: ModelCard
-}
+
+    data(){
+      return{
+      }
+    },
+
+    components:{
+      ModelCard
+      },
+
+    methods:{
+      getListofType(type){
+      var list
+      for(var element in this.$store.getters.getList){
+        if(element.type==type){
+          list.Add(element)
+        }
+      }
+      return list
+      }
+    }
+};
 </script>
 
 <style>
+.container{
+  min-height: 60px;
+  margin-top:20px;
+  background-color: #191919;  
+}
 
+.text_style{
+  margin: 20px;
+  color: #FFFFFF;
+}
+
+.row_style{
+  margin: 25px;
+}
+
+.col_style{
+  max-width: 300px;
+  margin-left: 10px;
+  margin-right: 10px;
+}
 </style>
