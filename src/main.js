8a2dc364427572eff305cc1a972a73c09528f063
guilherme.saracura <guilhermesaracura@gmail.com>
2022-10-11 22:12:09 -0300

    TO-25: Adicionado o recurso Axios para integração com a API

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import axios from 'axios';
 
+Vue.prototype.$http = axios;
 Vue.config.productionTip = false
 
 new Vue({
